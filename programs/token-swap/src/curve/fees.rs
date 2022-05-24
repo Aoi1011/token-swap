850b3d412bd7908e35bc26860857d6294de92d39
Aoi <ht.so.ik0102@gmail.com>
2022-05-24 11:01:27 +0700

    Create fees

--- programs/token-swap/src/curve/fees.rs
+++ programs/token-swap/src/curve/fees.rs
@@ -1,4 +1,5 @@
 use crate::errors::SwapError;
+use anchor_lang::solana_program::program_pack::{IsInitialized, Pack, Sealed};
 use anchor_lang::*;
 use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
 use std::convert::TryFrom;
@@ -120,3 +121,35 @@ impl Fees {
         Ok(())
     }
 }
+
+/// IsInitialized is required to use `Pack::pack` and `Pack::unpack`
+impl IsInitialized for Fees {
+    fn is_initialized(&self) -> bool {
+        true
+    }
+}
+
+// impl Sealed for Fees {}
+// impl Pack for Fees {
+//     const LEN: usize = 64;
+
+//     fn pack_into_slice(&self, output: &mut [u8]) {
+//         let output = array_mut_ref![output, 0, 64];
+//         let (
+//             trade_fee_numerator,
+//             trade_fee_denominator,
+//             owner_trade_fee_numerator,
+//             owner_trade_fee_denominator,
+//             owner_withdraw_fee_numerator,
+//             owner_withdraw_fee_denominator,
+//             host_fee_numerator,
+//             host_fee_denominator,
+//         ) = mut_array_refs![output, 8, 8, 8, 8, 8, 8, 8, 8];
+//         *trade_fee_numerator = self.trade_fee_numerator.to_le_bytes();
+
+//     }
+
+//     fn unpack_from_slice(input: &[u8]) -> Result<Fees> {
+//         Ok()
+//     }
+// }
