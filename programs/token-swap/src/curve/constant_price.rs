03cd8fbcbabc894b76061beba230a82d14dcd4d5
Aoi <ht.so.ik0102@gmail.com>
2022-05-25 06:31:30 +0700

    * Import all dependencies * Create function trading_tokens_to_pool_tokens (single asset deposit) * Implement CurveCalculator for ConstantPriceCurve

--- programs/token-swap/src/curve/constant_price.rs
+++ programs/token-swap/src/curve/constant_price.rs
@@ -1,1 +1,132 @@
-use crate::curve::calculator::map_zero_to_none;
+use {
+    crate::{
+        curve::calculator::{
+            map_zero_to_none, CurveCalculator, DynPack, RoundDirection, SwapWithoutFeesResult,
+            TradeDirection, TradingTokenResult,
+        },
+        errors::SwapError,
+    },
+    anchor_lang::solana_program::program_error::ProgramError,
+    arrayref::{array_mut_ref, array_ref},
+    spl_math::{checked_ceil_div::CheckedCeilDiv, precise_number::PreciseNumber, uint::U256},
+};
+
+// Single asset deposit
+pub fn trading_tokens_to_pool_tokens(
+    token_b_price: u64,
+    source_amount: u128,
+    swap_token_a_amount: u128,
+    swap_token_b_amount: u128,
+    pool_supply: u128,
+    trade_direction: TradeDirection,
+    round_direction: RoundDirection,
+) -> Option<u128> {
+    let token_b_price = U256::from(token_b_price);
+    let given_value = match trade_direction {
+        TradeDirection::AtoB => U256::from(source_amount),
+        TradeDirection::BtoA => U256::from(source_amount).checked_mul(token_b_price)?,
+    };
+    let tatal_value = U256::from(swap_token_b_amount)
+        .checked_mul(token_b_price)?
+        .checked_add(U256::from(swap_token_a_amount))?;
+    let pool_supply = U256::from(pool_supply);
+
+    match round_direction {
+        RoundDirection::Floor => Some(
+            pool_supply
+                .checked_mul(given_value)?
+                .checked_div(tatal_value)?
+                .as_u128(),
+        ),
+        RoundDirection::Ceiling => Some(
+            pool_supply
+                .checked_mul(given_value)?
+                .checked_ceil_div(tatal_value)?
+                .0
+                .as_u128(),
+        ),
+    }
+}
+
+#[derive(Clone, Debug, Default, PartialEq)]
+pub struct ConstantPriceCurve {
+    pub token_b_price: u64,
+}
+
+impl CurveCalculator for ConstantPriceCurve {
+    /// Constant price curve always returns 1:1
+    fn swap_without_fees(
+        &self,
+        source_amount: u128,
+        _swap_source_amount: u128,
+        _swap_destination_amount: u128,
+        trade_direction: TradeDirection,
+    ) -> Option<SwapWithoutFeesResult> {
+        let token_b_price = self.token_b_price as u128;
+
+        let (source_amount_swapped, destination_amount_swapped) = match trade_direction {
+            TradeDirection::BtoA => (source_amount, source_amount.checked_mul(token_b_price)?),
+            TradeDirection::AtoB => {
+                let destination_amount_swapped = source_amount.checked_div(token_b_price)?;
+                let mut source_amount_swapped = source_amount;
+
+                let remainder = source_amount_swapped.checked_rem(token_b_price)?;
+                if remainder > 0 {
+                    source_amount_swapped = source_amount.checked_sub(remainder)?;
+                }
+
+                (source_amount_swapped, destination_amount_swapped)
+            }
+        };
+
+        let source_amount_swapped = map_zero_to_none(source_amount_swapped)?;
+        let destination_amount_swapped = map_zero_to_none(destination_amount_swapped)?;
+        Some(SwapWithoutFeesResult {
+            source_amount_swapped,
+            destination_amount_swapped,
+        })
+    }
+
+    fn pool_tokens_to_trading_tokens(
+        &self,
+        pool_tokens: u128,
+        pool_token_supply: u128,
+        swap_token_a_amount: u128,
+        swap_token_b_amount: u128,
+        round_direction: RoundDirection,
+    ) -> Option<TradingTokenResult> {
+        let token_b_price = self.token_b_price as u128;
+        let total_value = self
+            .normalized_value(swap_token_a_amount, swap_token_b_amount)?
+            .to_imprecise()?;
+
+        let (token_a_amount, token_b_amount) = match round_direction {
+            RoundDirection::Floor => {
+                let token_a_amount = pool_tokens
+                    .checked_mul(total_value)?
+                    .checked_div(pool_token_supply)?;
+                let token_b_amount = pool_tokens
+                    .checked_mul(total_value)?
+                    .checked_div(token_b_price)?
+                    .checked_div(pool_token_supply)?;
+                (token_a_amount, token_b_amount)
+            }
+            RoundDirection::Ceiling => {
+                let (token_a_amount, _) = pool_tokens
+                    .checked_mul(total_value)?
+                    .checked_ceil_div(pool_token_supply)?;
+                let (pool_value_as_token_b, _) = pool_tokens
+                    .checked_mul(total_value)?
+                    .checked_ceil_div(token_b_price)?;
+                let (token_b_amount, _) =
+                    pool_value_as_token_b.checked_ceil_div(pool_token_supply)?;
+                (token_a_amount, token_b_amount)
+            }
+        };
+
+        Some(TradingTokenResult {
+            token_a_amount,
+            token_b_amount,
+        })
+    }
+}
