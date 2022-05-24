f558f8173de7018fd27d49e173ca137debf9ef4b
Aoi <ht.so.ik0102@gmail.com>
2022-05-24 11:02:50 +0700

    Move test case

--- programs/token-swap/src/curve/fees.rs
+++ programs/token-swap/src/curve/fees.rs
@@ -153,3 +153,24 @@ impl IsInitialized for Fees {
 //         Ok()
 //     }
 // }
+#[cfg(test)]
+mod tests {
+    use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
+
+    fn write_u16(bytes: &mut [u8; 2], num: u16) {
+        bytes[0] = num as u8;
+        bytes[1] = (num >> 8) as u8;
+    }
+
+    #[test]
+    fn test_arrayref() {
+        let mut data = [0, 1, 2, 3, 4, 0, 6, 7, 8, 9];
+        write_u16(array_mut_ref![data, 0, 2], 1);
+        println!("1: {:?}", data); // [1, 0, 2, 3, 4, 0, 6, 7, 8, 9]
+
+        write_u16(array_mut_ref![data, 2, 2], 5); // [1, 0, 5, 0, 4, 0, 6, 7, 8, 9]
+        println!("2: {:?}", data);
+
+        assert_eq!(*array_ref![data, 0, 4], [1, 0, 5, 0]);
+    }
+}
