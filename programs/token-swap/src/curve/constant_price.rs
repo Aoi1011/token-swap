9070caed5ed995d7b9b6648d259489154f257b29
Aoi <ht.so.ik0102@gmail.com>
2022-05-26 11:36:27 +0700

    Create new test case for withdraw case

--- programs/token-swap/src/curve/constant_price.rs
+++ programs/token-swap/src/curve/constant_price.rs
@@ -575,4 +575,49 @@ mod tests {
 
         }
     }
+
+    proptest! {
+        #[test]
+        fn curve_value_does_not_decrease_from_withdraw(
+            (pool_token_supply, pool_token_amount) in total_and_intermediate(),
+            swap_token_a_amount in 1..u64::MAX,
+            swap_token_b_amount in 1..u32::MAX,
+            token_b_price in 1..u32::MAX,
+        ) {
+            let curve = ConstantPriceCurve {token_b_price: token_b_price as u64};
+            let pool_token_amount = pool_token_amount as u128;
+            let pool_token_supply = pool_token_supply as u128;
+            let swap_token_a_amount = swap_token_a_amount as u128;
+            let swap_token_b_amount = swap_token_b_amount as u128;
+            let token_b_price = token_b_price as u128;
+
+            let value = curve.normalized_value(swap_token_a_amount, swap_token_b_amount).unwrap();
+
+            // make sure we trade at leat one of each token
+            prop_assume!(pool_token_amount * value.to_imprecise().unwrap() >= 2 * token_b_price * pool_token_supply);
+            prop_assume!(pool_token_amount <= pool_token_supply);
+
+            let withdraw_result = curve.pool_tokens_to_trading_tokens(
+                pool_token_amount,
+                pool_token_supply,
+                swap_token_a_amount,
+                swap_token_b_amount,
+                RoundDirection::Floor
+            ).unwrap();
+            prop_assume!(withdraw_result.token_a_amount <= swap_token_a_amount);
+            prop_assume!(withdraw_result.token_b_amount <= swap_token_b_amount);
+
+            let new_swap_token_a_amount = swap_token_a_amount - withdraw_result.token_a_amount;
+            let new_swap_token_b_amount = swap_token_b_amount - withdraw_result.token_b_amount;
+            let new_pool_token_supply = pool_token_supply - pool_token_amount;
+
+            let new_value = curve.normalized_value(new_swap_token_a_amount, new_swap_token_b_amount).unwrap();
+
+            let pool_token_supply = PreciseNumber::new(pool_token_supply).unwrap();
+            let new_pool_token_supply = PreciseNumber::new(new_pool_token_supply).unwrap();
+            assert!(new_value.checked_mul(&pool_token_supply).unwrap().greater_than_or_equal(&value.checked_mul(&new_pool_token_supply).unwrap()));
+
+
+        }
+    }
 }
